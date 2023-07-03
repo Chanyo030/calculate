8b27205fd2b4a6947561c198532321015eecce1f
Chanyo030 <d07520556@gmail.com>
2023-07-03 16:57:26 +0800

    calcuate

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script >
+import CalcView from "./components/Calc.vue";
+
+// !!!固定寫法!!
+// 輸出
+export default {
+
+  // 如同 key:value (ex. CalcView:CalcView)，同名可省略(如下)
+  components: {
+    CalcView
+  },
+
+  // 方法，方法裡return物件 (物件用冒號)
+  data() {
+    return {
+      name: "HRU"
+    }
+  },
+
+}
+</script>
+
+<template>
+  <!-- 下面那一行算是呼叫物件(帶入變數) -->
+  <!-- {{ !不可在網頁隨意更改值! }} -->
+  <h1>{{ name }}</h1>
+
+  <!-- v-model 雙向綁定 (帶入對應變數) !可在網頁隨意更改值! -->
+  <input type="text" v-model="name">
+
+  <CalcView />
+</template>
+
+<style scoped></style>
---
+++ src/components/Calc.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    data() {
+        return {
+            // 物件 - 宣告變數
+            num1: null,
+            num2: null,
+            sum: null
+        }
+    },
+    methods: {
+        calcSum() {
+            // 指向data方法裡的參數
+            // console.log("這是num1");
+            // console.log(this.num1);
+
+            // !用this才有辦法取上面的值!
+            this.sum = this.num1 + this.num2;
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        計算機
+    </div>
+    <input type="number" v-model="num1">
+    <p>+</p>
+    <input type="number" v-model="num2">
+
+    <!-- v-on:click 綁定按鈕 - 觸發事件 -->
+    <button type="button" v-on:click="calcSum">計算</button>
+    <h3>= {{ sum }}</h3>
+</template>
+
+<style></style>
